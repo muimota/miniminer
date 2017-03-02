85b4008d7fa5230d57f2ce8fa0c7658336669784
Your Name <martin.nadal@gmail.com>
2017-03-02 13:37:34 +0100

    calculates target and if hash is < target submits it to the pool

--- miner.py
+++ miner.py
@@ -3,16 +3,18 @@ import socket
 import json
 import hashlib
 import binascii
+from pprint import pprint
 import time
 import random
 
 address = '1Edh8qTf8H3xHQvKsFkQ39eNvHLN2URPU7'
 nonce   = hex(random.randint(0,2**32-1))[2:].zfill(8)
-
+host    = 'stratum.solo.nicehash.com'
+port    = 3334
 print "address:{} nonce:{}".format(address,nonce)
 
 sock    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('stratum.solo.nicehash.com', 3334))
+sock.connect((host,port))
 
 #server connection
 sock.sendall(b'{"id": 1, "method": "mining.subscribe", "params": []}\n')
@@ -22,21 +24,25 @@ sub_details,extranonce1,extranonce2_size = response['result']
 #authorize workers
 sock.sendall(b'{"params": ["'+str(address)+'", "password"], "id": 2, "method": "mining.authorize"}\n')
 
+#we will get a 4 line 
 response = ''
 while response.count('\n') < 4:
-    response += sock.recv(1024*3)
+    response += sock.recv(1024)
+
 #get rid of empty lines
 responses = [json.loads(res) for res in response.split('\n') if len(res.strip())>0]
-
+pprint(responses)
 #welcome message
 print responses[0]['params'][0]+'\n'
 
-try:
-    job_id,prevhash,coinb1,coinb2,merkle_branch,version,nbits,ntime,clean_jobs \
-        = responses[1]['params']
-except:
-    pprint(responses)
 
+job_id,prevhash,coinb1,coinb2,merkle_branch,version,nbits,ntime,clean_jobs \
+    = responses[1]['params']
+
+#target http://stackoverflow.com/a/22161019
+
+target = (nbits[2:]+'00'*(int(nbits[:2],16))).zfill(64)
+print 'target:{}\n'.format(target)
 
 extranonce2 = '00'*extranonce2_size
 
@@ -56,11 +62,19 @@ merkle_root = ''.join([merkle_root[i]+merkle_root[i+1] for i in range(0,len(merk
 print 'merkle_root:{}\n'.format(merkle_root)
 
 
-blockheader = version + prevhash + merkle_root + ntime + nonce +\
+blockheader = version + prevhash + merkle_root + nbits + ntime + nonce +\
     '000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000'
     
 print 'blockheader:\n{}\n'.format(blockheader)
 
 hash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(blockheader)).digest()).digest()
-print 'hash: {}'.format(binascii.hexlify(hash))
+hash = binascii.hexlify(hash)
+print 'hash: {}'.format(hash)
+
+if hash < target :
+    print 'successful mine!!'
+    payload = '{"params": ["'+address+'", "'+job_id+'", "'+extranonce2 \
+        +'", "'+ntime+'", "'+nonce+'"], "id": 1, "method": "mining.submit"}\n'
+    sock.sendall(payload)
+    print sock.recv(1024)
 sock.close()
